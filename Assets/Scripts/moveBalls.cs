a98730f54c7e38f1c840a919389662ab716f7e79
Pizmovc <pizmovc@gmail.com>
2015-06-16 01:00:50 +0200

    Disable switching cubes while paused and adjusted sizes and made cubes pillars

--- Assets/Scripts/moveBalls.cs
+++ Assets/Scripts/moveBalls.cs
@@ -3,18 +3,20 @@ using System.Collections;
 
 public class moveBalls : MonoBehaviour {
     public float speed;
+    public float duration = 4.0F;
+    private float startTime;
+    void Start()
+    {
+        startTime = Time.time;
+    }
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector2.down * speed * Time.deltaTime);
+        float t = (Time.time - startTime) / (10/speed);
+        transform.position = new Vector3(0, Mathf.SmoothStep(6, -4, t), 0);
 	}
 
-    public void ChangeSpeed(int newSpeed)
+    public void ChangeSpeed(float newSpeed)
     {
         speed = newSpeed;
     }
