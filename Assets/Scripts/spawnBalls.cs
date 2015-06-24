cf83ff82b817569cb848a09fd2a1e6ad22a69f79
Stamc <ziga.stamcar@gmail.com>
2015-06-24 20:23:18 +0200

    Add more sets of colors

--- Assets/Scripts/spawnBalls.cs
+++ Assets/Scripts/spawnBalls.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class spawnBalls : MonoBehaviour {
     public int numberOfBalls;
     public GameObject[] ballsPrefabs = new GameObject[5];
+    string[,] ballsColors = new string[2, 5] { { "FFAC00", "E83900", "FF00C6", "3900E8", "009CFF" }, { "BF3EFF", "385CE8", "4BFFF5", "38E83E", "FFFA3C" } };
     public float ballMoveSpeed;
     public int ballsToNextStage;
     public float ballSpeedIncrease;
@@ -28,6 +30,10 @@ public class spawnBalls : MonoBehaviour {
             numberOfBalls = 5;
             GameObject.FindWithTag("cubes").SendMessage("changeNumberOfBalls", numberOfBalls);
         }
+        if (counter == 0)
+            changeColors(0);
+        else if (counter == 10)
+            changeColors(1);
 
         GameObject ball;
 		if (gameTime >= delay) {
@@ -43,4 +49,31 @@ public class spawnBalls : MonoBehaviour {
 		}
         
 	}
+
+    void changeColors(int set)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Renderer rend = ballsPrefabs[i].GetComponent<Renderer>();
+            rend.sharedMaterial.SetColor("_Color", HexToRGB(ballsColors[set,i]));
+        }
+
+    }
+
+    Color HexToRGB (string hex)
+    {
+        Color color = new Color();
+
+        string red = hex.Substring(0, 2);
+        color.r = (float)int.Parse(red, System.Globalization.NumberStyles.HexNumber) / 255.0f;
+
+        string green = hex.Substring(2, 2);
+        color.g = (float)int.Parse(green, System.Globalization.NumberStyles.HexNumber) / 255.0f;
+
+        string blue = hex.Substring(4, 2);
+        color.b = (float)int.Parse(blue, System.Globalization.NumberStyles.HexNumber) / 255.0f;
+
+        color.a = 1.0f;
+        return color;
+    }
 }
